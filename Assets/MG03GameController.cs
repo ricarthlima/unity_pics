ad1b94147fc5e6f20cc4ccbad3e9393177626063
ricarthlima <rrsl@cin.ufpe.br>
2023-04-10 20:30:31 -0300

    animaçoes de aparicao e botoes de teste

--- Assets/MG03GameController.cs
+++ Assets/MG03GameController.cs
@@ -1,25 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MG03GameController : MonoBehaviour
 {
     [Header("Camera")]
     [SerializeField] private Camera m_Camera;
+    private bool animateCamera = false;
+    private bool isTouchedZoom = false;
 
     [Header("Layers")]
+    [SerializeField] private GameObject groupButtons;
     [SerializeField] private SpriteRenderer layerCadeirante;
     [SerializeField] private SpriteRenderer layerNanismo;
     [SerializeField] private SpriteRenderer layerPessoaCega;
     [SerializeField] private SpriteRenderer layerPessoaMuda;
     [SerializeField] private SpriteRenderer layerPortasLargas;
+    public enum Layers {Cadeirante, Nanismo, PessoaCega, PessoaMuda, PortasLargas};
+    SpriteRenderer toActiveSprite;
 
     [Header("Game")]
     public bool canInteract = false;
 
     void Start()
     {
-        
+        m_Camera.gameObject.transform.position = new Vector3(-1.51f, -3f, -10);
     }
 
     // Update is called once per frame
@@ -34,8 +40,110 @@ public class MG03GameController : MonoBehaviour
             if (!canInteract)
             {
                 canInteract = true;
+                groupButtons.SetActive(true);
             }
         }
         
+        
+        //Animate Camera
+        if (animateCamera)
+        {
+            if (!isTouchedZoom)
+            {
+                m_Camera.orthographicSize -= 1 * Time.deltaTime * 2;
+
+                if (m_Camera.orthographicSize <= 2.45f)
+                {
+                    isTouchedZoom = true;
+                    
+                }
+            }
+            else
+            {
+                if (toActiveSprite != null)
+                {
+                    LerpLayer();
+                }
+                else
+                {
+                    m_Camera.orthographicSize += 1 * Time.deltaTime * 2;
+                    if (m_Camera.orthographicSize >= 3.4f)
+                    {
+                        animateCamera = false;
+                        isTouchedZoom = false;
+                    }
+                }
+                
+            }
+        }
+
+
+    }
+    //  Nanismo, PessoaCega, PessoaMuda, PortasLargas, Reiniciar
+    public void OnClickCadeirante()
+    {
+        OnClickAnimation(Layers.Cadeirante);
+    }
+
+    public void OnClickNanismo()
+    {
+        OnClickAnimation(Layers.Nanismo);
+    }
+
+    public void OnClickPessoaCega()
+    {
+        OnClickAnimation(Layers.PessoaCega);
+    }
+
+    public void OnClickPessoaMuda()
+    {
+        OnClickAnimation(Layers.PessoaMuda);
+    }
+
+    public void OnClickPortasLargas()
+    {
+        OnClickAnimation(Layers.PortasLargas);
+    }
+
+    public void OnClickReiniciar()
+    {
+        layerCadeirante.color = new Color(1,1,1,0);
+        layerNanismo.color = new Color(1, 1, 1, 0);
+        layerPessoaCega.color = new Color(1, 1, 1, 0);
+        layerPessoaMuda.color = new Color(1, 1, 1, 0);
+        layerPortasLargas.color = new Color(1, 1, 1, 0);
+    }
+
+    private void OnClickAnimation(Layers layer)
+    {          
+        switch (layer)
+        {
+            case Layers.Cadeirante:
+                toActiveSprite = layerCadeirante;
+                break;
+            case Layers.Nanismo:
+                toActiveSprite = layerNanismo;
+                break;
+            case Layers.PessoaCega:
+                toActiveSprite = layerPessoaCega;
+                break;
+            case Layers.PortasLargas:
+                toActiveSprite = layerPortasLargas;
+                break;
+            case Layers.PessoaMuda:
+                toActiveSprite= layerPessoaMuda;
+                break;
+        }
+
+        animateCamera = true;
+    }
+
+    private void LerpLayer()
+    {
+        toActiveSprite.color = Color.Lerp(toActiveSprite.color, Color.white, Time.deltaTime * 10);
+        if (toActiveSprite.color == Color.white)
+        {
+            toActiveSprite = null;
+        }        
     }
 }
