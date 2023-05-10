204bbb4c6f7a40d5bddeff2e03f5cf856fadac6e
ricarthlima <rrsl@cin.ufpe.br>
2023-05-10 00:39:33 -0300

    mg05 - jogo da memoria concluido

--- Assets/Assets-MG05/Scripts/MG05GridController.cs
+++ Assets/Assets-MG05/Scripts/MG05GridController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MG05GridController : MonoBehaviour
 {
@@ -8,11 +11,8 @@ public class MG05GridController : MonoBehaviour
     [SerializeField] private MG05GameController gameController;
 
     [Header("Grids")]
-    public GameObject gridUI;
-
-    [Header("Cards Visíveis")]
-    [SerializeField] private GameObject cardBexiga; 
-    [SerializeField] private GameObject cardBoca, cardCoracao, cardEstomago, cardFigado, cardIntestino, cardMao, cardNariz, cardOlho, cardPe, cardPulmao, cardRins;
+    public GameObject[] listCards;
+    public GameObject[] listGridEscondidos;
 
     List<MG05PartesCorpo> listPartes = new List<MG05PartesCorpo>() {
     MG05PartesCorpo.Bexiga,
@@ -31,29 +31,122 @@ public class MG05GridController : MonoBehaviour
     MG05PartesCorpo.Rins,
     };
 
+    bool canClick;
+
     public void SortearGrid()
     {
-        //TODO: Esconder com GridEscondidos
-        //TODO: Embaralhar listPartes
-        //TODO: Gerar a depender da ordem, adicionar no gridUI
-        //TODO: Mostrar por um tempo (Remover GridEscondidos?)
-        //TODO: Cobrir com Escondidos
+        canClick = false;
+
+        // Esconder com GridEscondidos
+        foreach (GameObject cardEscondido in listGridEscondidos)
+        {
+            ToggleMostrarEscondidos(true);
+        }
+
+        // TODO: Embaralhar listPartes
+        // listPartes.Sort((a, b) => Random.Range(-1, 2));  
+        listPartes = RandomizeList(listPartes);
+
+        // Atualizar cards com a ordem gerada
+        for (int i = 0; i < listPartes.Count; i++)
+        {
+            listCards[i].GetComponent<MG05CardController>().ChangeParte(listPartes[i]);
+        }
+
+        // Mostrar por um tempo (Remover GridEscondidos?)
+        foreach (GameObject cardEscondido in listGridEscondidos)
+        {
+            ToggleMostrarEscondidos(false);
+        }
+
+        // Cobrir com Escondidos
+        StartCoroutine(EspereParaEsconder(5));
     }
 
     public void ButtonClicked(int posicao)
     {
-        //TODO: Sumir com o escondido específico
-
-        bool isCorrect = gameController.ClickedMemoria(listPartes[posicao]);
-
-        if (!isCorrect)
+        if (canClick)
         {
-            CardErrado(posicao);
+            canClick = false;
+
+            // Sumir com o escondido específico
+            listGridEscondidos[posicao].GetComponent<Image>().enabled = false;
+
+            bool isCorrect = gameController.ClickedMemoria(listPartes[posicao]);
+
+            if (!isCorrect)
+            {
+                StartCoroutine(CardErrado(posicao));
+            }
+        }
+        
+    }
+
+    IEnumerator CardErrado(int posicao)
+    {
+        // Voltar com escondido e deixar vermelho
+        yield return new WaitForSeconds(1);
+        listGridEscondidos[posicao].GetComponent<Image>().enabled = true;
+        listGridEscondidos[posicao].GetComponent<Image>().color = Color.red;
+        canClick = true;
+    }
+
+    void ToggleMostrarEscondidos(bool show)
+    {
+        foreach (GameObject cardEscondido in listGridEscondidos)
+        {
+            cardEscondido.SetActive(show);
+            if (show)
+            {
+                cardEscondido.GetComponent<Image>().enabled = true;
+                cardEscondido.GetComponent<Image>().color = Color.white;
+            }
         }
     }
 
-    public void CardErrado(int posicao)
+    IEnumerator EspereParaEsconder(int seconds)
     {
-        //TODO: Voltar com escondido e deixar vermelho
+        yield return new WaitForSeconds(seconds);
+        ToggleMostrarEscondidos(true);
+        canClick = true;
+    }
+
+    List<MG05PartesCorpo> RandomizeList(List<MG05PartesCorpo> list)
+    {
+        List<MG05PartesCorpo> result = new List<MG05PartesCorpo>() { 
+            MG05PartesCorpo.Bexiga, 
+            MG05PartesCorpo.Bexiga, 
+            MG05PartesCorpo.Bexiga, 
+            MG05PartesCorpo.Bexiga, 
+            
+            MG05PartesCorpo.Bexiga, 
+            MG05PartesCorpo.Bexiga, 
+            MG05PartesCorpo.Bexiga, 
+            MG05PartesCorpo.Bexiga, 
+            
+            MG05PartesCorpo.Bexiga,
+            MG05PartesCorpo.Bexiga,
+            MG05PartesCorpo.Bexiga,
+            MG05PartesCorpo.Bexiga,
+        };
+
+        List<int> positions = new List<int>();
+
+        foreach (MG05PartesCorpo parte in list)
+        {
+            while (true)
+            {
+                int index = Random.Range(0, list.Count);
+                if (!positions.Contains(index))
+                {
+                    result[index] = parte;
+                    positions.Add(index);
+                    break;
+                }
+
+            }
+        }
+
+        return result;
     }
 }
